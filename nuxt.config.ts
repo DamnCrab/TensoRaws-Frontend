7fe91d92fff322f135d17b983aec74f09bb3d2c5
DamnCrab <enormouscrab@gmail.com>
2024-12-09 16:09:52 +0800

    init&test deploy

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt',
+    '@formkit/auto-animate',
+    'dayjs-nuxt',
+    'nuxt-lodash',
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+})
