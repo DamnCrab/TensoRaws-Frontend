b0f71db272dea7ce5fe9ad6484622089727a87f4
DamnCrab <enormouscrab@gmail.com>
2024-12-09 16:34:21 +0800

    ✨ feat(`pages/user.vue`): 添加用户页面并设置身份验证中间件。 ✨ feat(`nuxt.config.ts`): 配置页面过渡动画效果。 ✨ feat(`pages/login.vue`): 创建登录页面。 ✨ feat(`app.vue`): 整合页面结构并优化过渡样式。 ✨ feat(`middleware/auth.ts`): 实现身份验证中间件。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
---
+++ pages/user.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth'
+})
+</script>
+
+<template>
+<div>user</div>
+</template>
+
+<style scoped>
+
+</style>
